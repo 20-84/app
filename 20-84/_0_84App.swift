293990df3f594ad0c6a8b144d79e6466b897dd01
vauxhall <zero.griffin@gmail.com>
2020-10-16 14:34:47 +0200

    Initial Commit

---
+++ 20-84/_0_84App.swift
@@ -0,0 +1,17 @@
+//
+//  _0_84App.swift
+//  20-84
+//
+//  Created by vaux on 16.10.20.
+//
+
+import SwiftUI
+
+@main
+struct _0_84App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
