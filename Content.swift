a0d6f8abd224ed42a66b6d6c17a130b048cb0ca7
vauxhall <zero.griffin@gmail.com>
2020-10-18 17:30:04 +0200

    Add images

--- Content.swift
+++ Content.swift
@@ -1,8 +1,28 @@
 import SwiftUI
 
 struct Content: View {
+    @State private var index = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("c00_\(index < 10 ? "0\(index)" : "\(index)")")
+            VStack {
+                Spacer()
+                HStack {
+                    Button {
+                        index -= 1
+                    } label: {
+                        Image(systemName: "chevron.left.circle.fill")
+                            .font(.title)
+                    }.padding()
+                    Spacer()
+                    Button {
+                        index += 1
+                    } label: {
+                        Image(systemName: "chevron.right.circle.fill")
+                            .font(.title)
+                    }.padding()
+                }
+            }
+        }
     }
 }
